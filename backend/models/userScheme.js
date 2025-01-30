6f805ef1977d6354074fb78ce47f6024b25b1a44
Abhishek kumar jha <Abhishekkrjha34@gmail.com>
2025-01-31 02:04:47 +0530

    backend /controllers/user login logout added

--- backend/models/userScheme.js
+++ backend/models/userScheme.js
@@ -8,7 +8,7 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: [true,"Please provide your name!"],
         minLength: [3,"Name must be at least 3 characters!"],
-        maxLenght:[30,"Name cannt be exceed 30 characters!"],
+        maxLength:[30,"Name cannot exceed 30 characters!"],
     },
     email:{
         type: String,
@@ -23,7 +23,8 @@ const userSchema = new mongoose.Schema({
         type:String,
         required:[true,"Please enter the password !"],
         minLength: [8,"Password must be at least 8 characters!"],
-        maxLenght:[32,"Password cannt be exceed 32 characters!"],
+        maxLength:[32,"Password cannot exceed 32 characters!"],
+        select:false,
     },
     role:{
         type:String,
@@ -55,9 +56,9 @@ userSchema.methods.comparePassword = async function (enteredPassword) {
 
 //generating a jwt token for authorization
 
-userSchema.method.getJWTToken = function(){
-    return jwt.sign({id:this._id},process.env.JWT_SECRET_KEY,{
-        expiresIn:process.env.JWT_EXPIRE,
+userSchema.methods.getJWTToken = function(){
+    return jwt.sign({id:this._id}, process.env.JWT_SECRET_KEY,{
+        expiresIn: process.env.JWT_EXPIRE || '1h',
     });
 
 };
--- backend/routes/userRouter.js
+++ backend/routes/userRouter.js
@@ -1,8 +1,11 @@
 import express from "express";
-import {register} from '../controllers/userControllers.js';
+import {login, logout, register} from '../controllers/userControllers.js';
 
 
 const router = express.Router();
+
 router.post("/register",register);
+router.post("/login",login);
+router.get("/logout",logout);
 
 export default router;
